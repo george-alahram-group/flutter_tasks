7df55a34171f750582852b01a9eb5d5fbae2e8d6
Al-Ahram <george.nady@alahramgroup-eg.net>
2021-04-26 12:42:12 +0200

    MessageCard

--- lib/custom_widget/widgets/my_cart/MyCart.dart
+++ lib/custom_widget/widgets/my_cart/MyCart.dart
@@ -54,13 +54,6 @@ class MyCart extends StatelessWidget {
           ],
         ),
       ),
-      Padding(
-        padding: EdgeInsets.all(15),
-        child: CustomPaint(
-          size: Size(MediaQuery.of(context).size.width, (MediaQuery.of(context).size.width/1.5).toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
-          painter: MessageCard(),
-        ),
-      )
     ],
   );
 }
